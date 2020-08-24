4eb483c0237f43a1458ad315660709a8c2fe7a72
merkyl01 <Bostyn18!>
2020-08-24 08:03:55 -0500

    Fixing models that had BIGINT to go to Integer. Also fixed DateTime in Users.

--- bot/database/models/profile.py
+++ bot/database/models/profile.py
@@ -5,4 +5,4 @@ class Profile(db.Model):
     __tablename__ = "Profile"
 
     user_id = db.Column(db.BIGINT, primary_key=True)
-    badge_id = db.Column(db.BIGINT)
+    badge_id = db.Column(db.Integer)
--- bot/database/models/users.py
+++ bot/database/models/users.py
@@ -6,9 +6,9 @@ class Users(db.Model):
 
     id = db.Column(db.BIGINT, primary_key=True)
     balance = db.Column(db.BIGINT)
-    user_xp = db.Column(db.BIGINT)
-    user_level = db.Column(db.BIGINT)
+    user_xp = db.Column(db.Integer)
+    user_level = db.Column(db.Integer)
     farm_id = db.Column(db.BIGINT)
-    energy = db.Column(db.BIGINT)
-    last_visit = db.Column(db.Date)
+    energy = db.Column(db.Integer)
+    last_visit = db.Column(db.DateTime)
     allow_notif = db.Column(db.Bool)
