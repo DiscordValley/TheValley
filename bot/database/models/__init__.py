b7e6791c8dbf317de5ca3430ce91d13b8b43f8f1
ferociouskyle <kylejosephmercier@gmail.com>
2020-08-27 16:43:42 -0500

    Revert "WIP: Add basic farming class "

--- bot/database/models/__init__.py
+++ bot/database/models/__init__.py
@@ -1,4 +1,4 @@
 from .guilds import Guild  # noqa
 from .profile import Profile  # noqa
 from .users import Users  # noqa
-from .farms import Farm  # noqa
+from .farm import Farm  # noqa
--- bot/database/models/farm.py
+++ bot/database/models/farm.py
@@ -2,7 +2,7 @@ from bot.database import db
 
 
 class Farm(db.Model):
-    __tablename__ = "farms"
+    __tablename__ = "Farm"
 
     farm_id = db.Column(db.BIGINT, primary_key=True)
     user_id = db.Column(db.BIGINT)
