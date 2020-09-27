db73f529df4d26b19fe3b54d1202c754e99c6636
Daniel Rose <daniel@whitehatcat.net>
2020-09-27 15:29:05 -0400

    Add Last Watered and Stage

--- bot/database/models/planted_crops.py
+++ bot/database/models/planted_crops.py
@@ -8,6 +8,8 @@ class PlantedCrop(db.Model):
     id = db.Column(db.BIGINT, primary_key=True)
     farm_id = db.Column(db.ForeignKey("farms.id"))
     crop_id = db.Column(db.BIGINT)  # Will be ForeignKey of crops.id, doesn't exist yet
+    stage = db.Column(db.Integer, default=1)
+    last_watered = db.Column(db.DateTime)
     planted_at = db.Column(db.DateTime(), default=datetime.utcnow())
     coord_column = db.Column(db.Integer)
     coord_row = db.Column(db.Integer)
