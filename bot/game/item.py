188776eaefa8fd5659e9e34da2ce9dc517e2082d
makubob <makupi@icloud.com>
2020-09-28 18:37:11 +0200

    Add item loading

--- bot/game/item.py
+++ bot/game/item.py
@@ -1,4 +1,7 @@
 from dataclasses import dataclass
+from typing import Optional
+
+from bot.database.models.items import Item as ItemModel
 
 
 @dataclass
@@ -6,18 +9,28 @@ class InventoryItem:
     inventory_id: int
     item_id: int
     quantity: int
-    name: str
-    description: str
-    cost: int
-    creation_id: int
+    name: Optional[str] = None
+    description: Optional[str] = None
+    cost: Optional[int] = None
+    creation_id: Optional[int] = None
 
     @classmethod
     async def load(
         cls, inventory_id: int, item_id: int, quantity: int
     ) -> "InventoryItem":
         """Fully load a Inventory item with information from items table"""
-        pass
+        item = InventoryItem(
+            inventory_id=inventory_id, item_id=item_id, quantity=quantity
+        )
+        await item.fetch()
+        return item
 
     async def fetch(self):
         """Fetch information from items table if not previously loaded"""
-        pass
+        item = await ItemModel.get(self.item_id)
+        if not item:
+            raise ValueError(f"Item with id {self.item_id} not found in database.")
+        self.name = item.name
+        self.description = item.description
+        self.cost = item.cost
+        self.creation_id = item.creation_id
