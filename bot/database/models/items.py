1ba613ee775c083be5061873a29c6aa781f13406
makubob <makupi@icloud.com>
2020-09-28 18:32:01 +0200

    Change db tables inventories and items
    item_id now ForeignKey to items.id
    items table doesn't need item_id
    items.name is unique

--- bot/database/models/items.py
+++ bot/database/models/items.py
@@ -5,7 +5,6 @@ class Item(db.Model):
     __tablename__ = "items"
 
     id = db.Column(db.BIGINT, primary_key=True)
-    item_id = db.Column(db.BIGINT)
     name = db.Column(db.Text)
     description = db.Column(db.Text)
     cost = db.Column(db.Integer)
