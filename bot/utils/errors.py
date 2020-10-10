cc4b4fa6cf41c6dd10ab2f0addeadf7d94c9c4df
makubob <makupi@icloud.com>
2020-10-10 16:12:10 +0200

    Add inventory methods
    get_item, find_item, remove_item, add_item

--- bot/utils/errors.py
+++ bot/utils/errors.py
@@ -6,3 +6,10 @@ class ItemNotFoundError(Exception):
 class InvalidQuantityError(Exception):
     def __init__(self, name: str):
         self.name = name
+
+
+class InsufficientQuantityError(Exception):
+    def __init__(self, name: str, current_quantity: int, remove_quantity: int):
+        self.name = name
+        self.current_quantity = current_quantity
+        self.remove_quantity = remove_quantity
