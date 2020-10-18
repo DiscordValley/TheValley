b864ce03bd959609bffcd1ca5bef511784fecaf3
HRAND5 <hrand5@hrand5-pop-os.home>
2020-10-18 18:33:05 +0200

    Change inventory format. Add paginator.

--- bot/cogs/profile.py
+++ bot/cogs/profile.py
@@ -1,6 +1,12 @@
 import discord
 from discord.ext import commands
 from bot.game import Player, Farm
+from disputils import BotEmbedPaginator
+
+
+def divide_chunks(lis, n):
+    for i in range(0, len(lis), n):
+        yield lis[i : i + n]
 
 
 class Profile(commands.Cog):
@@ -34,12 +40,45 @@ class Profile(commands.Cog):
         player = await Player.load(
             user_id=ctx.author.id, guild_id=ctx.guild.id, load_inventory=True
         )
-        embed = discord.Embed(title="Inventory")
-        embed.set_footer(text=f"Inventory of {ctx.author}")
-        for item in player.inventory.items.values():
-            embed.add_field(name=item.name.capitalize(), value=str(item.quantity))
+        items = player.inventory.items.values()
 
-        await ctx.send(embed=embed)
+        if items:
+            if len(items) > 5:
+                pages = list(divide_chunks(list(items), 5))
+                embeds = []
+                for p in pages:
+                    embed = discord.Embed(
+                        title="Inventory",
+                    )
+                    embed.set_footer(text=f"Inventory of {ctx.author}")
+
+                    for item in p:
+                        embed.add_field(
+                            name=f"**{item.quantity} - {item.name.capitalize()} - {item.cost}**",
+                            value=item.description,
+                            inline=False,
+                        )
+                    embeds.append(embed)
+                paginator = BotEmbedPaginator(ctx, embeds)
+                await paginator.run()
+            else:
+                embed = discord.Embed(
+                    title="Inventory",
+                )
+                embed.set_footer(text=f"Inventory of {ctx.author}")
+                print("there has been a mistake")
+                for item in items:
+                    embed.add_field(
+                        name=f"**{item.quantity} - {item.name.capitalize()} - {item.cost}**",
+                        value=item.description,
+                        inline=False,
+                    )
+                await ctx.send(embed=embed)
+        else:
+            embed = discord.Embed(
+                title="Inventory", description="Your inventory is empty."
+            )
+            await ctx.send(embed=embed)
 
 
 def setup(bot):
